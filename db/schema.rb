b51398053ef5cb10131bbcd84ac935f15cf77e18
Max Cantor <max@maxcantor.net>
2015-12-07 18:18:35 -0500

    Switch to SQLite3 from PostgreSQL.

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151202061742) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "authors", force: :cascade do |t|
     t.string   "name"
     t.string   "phone_number"
