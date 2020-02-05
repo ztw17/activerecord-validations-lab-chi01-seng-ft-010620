f0057db7c2069cac1ccf257c4556bbcd6dc0ce75
Zach Weber <weberzt@gmail.com>
2020-02-05 14:34:11 -0600

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true
+    validates :phone_number, length: {is: 10}
+    validates :name, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :title_must_be_clickbait
+
+    def title_must_be_clickbait
+        array = ["Won't Believe", "Secret", "Top /[0-9]/", "Guess"]
+        if title.present? && !array.any? {|cb| title.include?(cb)}
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
